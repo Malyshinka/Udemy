982b7b7686a83c662c8b2a743792f71765ea7725
malyshinka <hmalyshkina@gmail.com>
2020-07-29 19:21:09 +0200

    Udemy Java Course

--- src/com/udemy/domainmodel/Employee.java
+++ src/com/udemy/domainmodel/Employee.java
@@ -2,26 +2,24 @@ package com.udemy.domainmodel;
 
 public class Employee {
 
-	private static int id = 1000;
+	private static int id;
 	private int employeeId;
 	private String name;
 	private String position;
 	private int salary;
 	private String department;
-	
+
 	static {
 		id = 1001;
 		System.out.println("Static init block is called");
 	}
-	
+
 	{
 		department = "IT";
 		System.out.println("Non-static init block is called");
-	 	
+
 	}
-	
-	
-	
+
 	public Employee() {
 		this("A", "B", 1);
 		System.out.println("Empry constructor called");
@@ -31,9 +29,9 @@ public class Employee {
 		this(name, position, salary, "IT");
 		System.out.println("Constructor with 3 params called");
 	}
-	
+
 	private Employee(String name, String position, int salary, String department) {
- 		employeeId = id ++;
+		employeeId = id++;
 		this.name = name;
 		this.position = position;
 		this.salary = salary;
@@ -56,6 +54,7 @@ public class Employee {
 	public int getSalary() {
 		return salary;
 	}
+
 	public int getEmployeeId() {
 		return employeeId;
 	}
@@ -68,16 +67,10 @@ public class Employee {
 		this.department = department;
 	}
 
-
-
-	/*@Override
-	public String toString() {
-		return "Employee [employeeId=" + employeeId + ", name=" + name + ", position=" + position + ", salary=" + salary
-				+ "]";
-	}*/
-
-	
-	
-	
+	/*
+	 * @Override public String toString() { return "Employee [employeeId=" +
+	 * employeeId + ", name=" + name + ", position=" + position + ", salary=" +
+	 * salary + "]"; }
+	 */
 
 }
